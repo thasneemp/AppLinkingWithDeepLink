c2f27a4f63965f337c79517fabb07dad1a26b912
thasneem <thasneem@farabi.ae>
2018-09-30 15:25:35 +0400

    deep linking completed

--- app/src/main/java/com/example/thasneem/deeplinkingwithapplink/MainActivity.java
+++ app/src/main/java/com/example/thasneem/deeplinkingwithapplink/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.thasneem.deeplinkingwithapplink;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = getIntent();
+        Uri data = intent.getData();
+        if (data != null) {
+            Toast.makeText(this, data.getQueryParameter("name"), Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
